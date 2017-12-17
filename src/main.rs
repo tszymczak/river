9429c169af9c06e709cbe43049862b35b9803c4c
Thomas Szymczak <you@example.com>
2017-12-17 09:48:50 -0500

    Break the code into functions.

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,21 @@
 use std::io;
 use std::path::Path;
 extern crate image;
-use image::{GenericImage, Pixel, FilterType};
+use image::{GenericImage, Pixel, FilterType, ImageResult, ImageBuffer, DynamicImage};
 
 fn main() {
+    // Get the input file name.
+    let infile_name = read_infile_name();
+
+    // Open the input image file and resize it.
+    let inimg = image::open(&Path::new(&infile_name)).ok().expect("Opening image failed");
+    let img = inimg.resize(32, 32, FilterType::Nearest);
+
+    // Render the image to the terminal.
+    render(img);
+}
+
+fn read_infile_name() -> String {
     // Read the input file name from the user.
     let mut infile_name = String::new();
     println!("Enter input file name:");
@@ -19,14 +31,13 @@ fn main() {
     // Source: https://stackoverflow.com/questions/37888042/remove-single-trailing-newline-from-string-without-cloning
     let new_length = infile_name.trim_right().len();
     infile_name.truncate(new_length);
+    return infile_name;
+}
 
-    // Open the input image file.
-    let inimg = image::open(&Path::new(&infile_name)).ok().expect("Opening image failed");
-    let img = inimg.resize(32, 32, FilterType::Nearest);
+// Print the input image file.
+fn render(img: image::DynamicImage) {
     let (width, height) = img.dimensions();
     println!("{} {}", width, height);
-
-    // Print the input image file.
     for y in 0..height {
         for x in 0..width {
             let this_pixel = img.get_pixel(x, y);
