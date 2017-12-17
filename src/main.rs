07b8587901e02fe76be1f6cce2c64f669269dc71
Thomas Szymczak <you@example.com>
2017-12-17 10:40:29 -0500

    Start parsing command line arguments.

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,38 @@
 use std::io;
 use std::path::Path;
 extern crate image;
-use image::{GenericImage, Pixel, FilterType, ImageResult, ImageBuffer, DynamicImage};
+use image::{GenericImage, Pixel, FilterType};
+extern crate clap;
+use clap::{App, Arg};
+use std::process;
+
 
 fn main() {
+    // Parse command line input.
+    let matches = App::new("River")
+        .version("0.1")
+        .about("Print images in the Terminal using text characters.")
+        .author("Thomas Szymczak")
+        .arg(Arg::with_name("INPUT")
+            .help("The name of the input file")
+            .required(true)
+            .index(1))
+        .arg(Arg::with_name("style")
+            .help("What style to use when printing the image. This determines how it looks.")
+            .short("s")
+            .possible_values(&["pound"]))
+       .get_matches();
+
     // Get the input file name.
-    let infile_name = read_infile_name();
+    let mut infile_name = "";
+    if matches.is_present("INPUT") {
+        println!("Input file is {}", matches.value_of("INPUT").unwrap());
+        infile_name = matches.value_of("INPUT").unwrap();
+    } else {
+        println!("No input file name supplied!");
+        infile_name = "";
+        process::exit(1);
+    }
 
     // Open the input image file and resize it.
     let inimg = image::open(&Path::new(&infile_name)).ok().expect("Opening image failed");
@@ -20,19 +47,6 @@ fn main() {
     render(img);
 }
 
-fn read_infile_name() -> String {
-    // Read the input file name from the user.
-    let mut infile_name = String::new();
-    println!("Enter input file name:");
-    io::stdin().read_line(&mut infile_name).expect("Failed to read input file name.");
-    println!("Reading input from {}", infile_name);
-
-    // Cut the newline off the read string.
-    // Source: https://stackoverflow.com/questions/37888042/remove-single-trailing-newline-from-string-without-cloning
-    let new_length = infile_name.trim_right().len();
-    infile_name.truncate(new_length);
-    return infile_name;
-}
 
 // Print the input image file.
 fn render(img: image::DynamicImage) {
