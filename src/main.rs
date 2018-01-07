36977c99870287b9536277386b1f93089472a943
Thomas Szymczak <you@example.com>
2018-01-07 12:19:43 -0500

    License code under the GPLv3.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 // A Rust program that prints images to the terminal. A fun, if useless,
 // project that taught me to code in Rust.
+
+// Copyright 2018 Thomas Szymczak
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 use std::path::Path;
 use std::process;
 use std::{f32, i32};
